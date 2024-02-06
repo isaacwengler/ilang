a1bf9610335a3cc5b955ba99876f571ade703790
Isaac Wengler <iwengler3@gmail.com>
2024-02-05 23:18:12 -0600

    parser does some parsing

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module ilang
 
 go 1.21.5
 
-require github.com/antlr/antlr4/runtime/Go/antlr v1.4.10
+require github.com/antlr4-go/antlr/v4 v4.13.0
+
+require golang.org/x/exp v0.0.0-20230515195305-f3d0a9c9a5cc // indirect
--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
-import "ilang/logger"
+import (
+	parser "ilang/generated"
+	"ilang/logger"
+
+	"github.com/antlr4-go/antlr/v4"
+)
 
 func main() {
-    logger.Debug("only if debug flag")
-    logger.Error("eorrorororor")
-    logger.Warn("lsdkfj")
+    logger.Debug("Starting ilang")
+
+    inputStream := antlr.NewInputStream("let hi = \"hello\";")
+    lexer := parser.Newilang_lexer(inputStream)
+    tokenStream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+    
+    p := parser.Newilang_parser(tokenStream)
+
+    antlr.ParseTreeWalkerDefault.Walk(&antlr.BaseParseTreeListener{}, p.Start_())
 }
